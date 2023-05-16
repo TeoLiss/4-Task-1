042df1e5c18a75ebe31404996a4eb131ce0f0789
Teo Liss <66493826+TeoLiss@users.noreply.github.com>
2023-05-16 20:42:13 +0300

    Add files via upload

---
+++ 4 Task 2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выведи матрицу из случайных чисел");
+           
+            Console.WriteLine("Введи число строк:");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введи число столбцов:");
+            int j = int.Parse(Console.ReadLine());
+            int[,] matrixOne;
+            int[,] matrixTwo;
+            matrixOne = new int[k, j];
+            matrixTwo = new int[k, j];  
+            Random rand = new Random();
+            Console.WriteLine("Матрицы А \t\t\t\t Матрица В");
+            Console.Write($"\n\n");
+            for (int i = 0; i < k; i++)
+            {
+                for (int n = 0; n < j; n++)
+                {
+                    matrixOne[i, n] = rand.Next(0, 1000);
+                    Console.Write($"{matrixOne[i, n]}\t");
+                }
+                Console.Write($"\t\t");
+             for (int n = 0; n < k; n++)
+             {
+                    matrixTwo[i,n] = rand.Next(1, 1000);   
+                    Console.Write($"{matrixTwo[i,n]}\t");
+              }
+                Console.WriteLine();
+            }
+            int[,] matrixSum;
+            matrixSum = new int[k, j];
+            Console.Write($"\n\n");
+            Console.WriteLine("Матрица С (Сумма А и В)");
+            Console.Write($"\n\n");
+            for (int i = 0; i < k; i++)
+            {
+                for (int n = 0;n < j; n++)
+                {
+                    matrixSum[i, n] = matrixOne[i,n] + matrixTwo[i,n];
+                    Console.Write($"{matrixSum[i, n]}\t");
+                }
+                Console.Write($"\t\t");
+                Console.WriteLine();    
+            }
+            Console.ReadKey();  
+        }
+        }
+    }
+
+
---
+++ 4 Task 2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("4 Task 2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4 Task 2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bfae0b44-29fd-4c63-b10b-4a6268ac891a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
